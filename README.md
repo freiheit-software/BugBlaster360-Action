80c68dc2343fa8a84b3ec1f2fa6abf6ca7ff9285
Tobias Bambullis <tobias.bambullis@facilioo.de>
2023-06-16 22:51:04 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# BugBlaster360-Action
+
+This GitHub Action transfers the gen2 log to a Go Webservice which generates the outputs to a nice summary
+
+### Parameters
+
+``commit`` the reference of the commit
+
+``file`` Path to logfile from gen2 tests
---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+ const commit = core.getInput('commit');
+ console.log(commit); 
+
+ const file = core.getInput('file');
+ console.log(file); 
+
+} catch (error) {
+  core.setFailed(error.message);
+}
