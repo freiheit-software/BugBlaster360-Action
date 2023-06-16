58e171af6baa6de0082d7a8e16c9fdeb4885c1b2
Tobias Bambullis <tobias.bambullis@facilioo.de>
2023-06-17 01:21:49 +0200

    upload files to facilioo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
+const fs = require('fs').promises;
 const FormData = require('form-data');
 const axios = require('axios');
 
@@ -15,43 +15,39 @@ try {
  const formData = new FormData();
  formData.append("commit", commit);
 
- fs.readdir(directory, (err, files) => {
+ await fs.readdir(directory, (err, files) => {
    files.forEach(file => {
 
      const ext = file.split('.');
      if(ext[ext.length - 1] == 'trx') {
 
-      var fileBuffer = fs.createReadStream(directory + '/' + file);
-      fileBuffer.on('data', function (chunk) {
-
-      	formData.append("file", chunk);
-      	console.log("file included: " + directory + '/' + file);
-
-      }); 
+      let data = await fs.readFile(directory + '/' + file);
+      formData.append("file", data);
+      console.log("file included: " + directory + '/' + file);
 
      }
 
    });
 
-   console.log(formData);
+ });
 
-   var config = {
-    method: 'post',
-    url: 'https://app.facilioo.de/jaromarcrebekka.php',
-    headers: { 
-      ...formData.getHeaders()
-    },
-    data : formData
-   };
+ console.log(formData);
 
-   axios(config).then(function (response) {
-    console.log("request successful!");
-    console.log(JSON.stringify(response.data));
-   }).catch(function (error) {
-    console.log("request failed!");
-    console.log(error);
-   });
+ var config = {
+  method: 'post',
+  url: 'https://app.facilioo.de/jaromarcrebekka.php',
+  headers: { 
+    ...formData.getHeaders()
+  },
+  data : formData
+ };
 
+ axios(config).then(function (response) {
+  console.log("request successful!");
+  console.log(JSON.stringify(response.data));
+ }).catch(function (error) {
+  console.log("request failed!");
+  console.log(error);
  });
 
 } catch (error) {
